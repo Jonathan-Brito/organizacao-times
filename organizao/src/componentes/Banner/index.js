f90271d1124186b0ec309cebdad5d03f1c100856
Jonathan Brito <devjonathanbrito@gmail.com>
2023-04-01 13:37:49 -0300

    Adicionado os componentes e imagens

---
+++ organizao/src/componentes/Banner/index.js
@@ -0,0 +1,10 @@
+import './banner.css'
+
+const Banner = () => {
+    return (<header className="cabecalho">
+        <img src="/imagens/banner.png" alt="Logo do Oraganizador" />
+
+    </header>)
+}
+
+export default Banner;
